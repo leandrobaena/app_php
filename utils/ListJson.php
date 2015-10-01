ae2b165accb82899e0525c958bff2e2e46ebf708
Leandro <Leandro@Leandro>
2015-10-01 13:08:59 -0500

    base

---
+++ utils/ListJson.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace utils;
+
+/**
+ * Lista especializada para el manejo de objetos que se exportan en formato JSON
+ *
+ * @author Leandro Baena Torres
+ */
+class ListJson {
+    
+    private $total;
+}
